25aa71383e91e77286d6ae618ec78210571eda6d
samuelhe864 <samuelhe52@outlook.com>
2024-06-30 16:46:32 +0800

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Samuel He on 2024/6/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Samuel He on 2024/6/30.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
