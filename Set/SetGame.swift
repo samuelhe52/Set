77bf5da7d555f5098115b85dfaefe50b9f8b3f17
samuelhe864 <samuelhe52@outlook.com>
2024-07-07 12:20:07 +0800

    Added SetGame.swift

--- Set/SetGame.swift
+++ Set/SetGame.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct SetGame {
+    var deck: [SetCard] = createDeck()
+    
+    
+    // MARK: - static methods
+    private static func createDeck() -> [SetCard]{
+        var tmpDeck: Array<SetCard> = []
+        
+        for number in SetCard.ShapeCount.allCases {
+            for cardShape in SetCard.Shapes.allCases {
+                for shading in SetCard.Shadings.allCases {
+                    tmpDeck.append(SetCard(shape: cardShape, shapeCount: number, shading: shading, color: .pink))
+                }
+            }
+        }
+        
+        return tmpDeck
+    }
+}
