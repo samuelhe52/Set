d97f190c3921c7ea320155460d2df25605c92578
samuelhe864 <samuelhe52@outlook.com>
2024-07-12 17:40:24 +0800

    Renamed: SetShape.swift -> SetShapes.swift Created: VerticalLinesOverlay.swift

---
+++ Set/VerticalLinesOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  VerticalLinesOverlay.swift
+//  Set
+//
+//  Created by Samuel He on 2024/7/12.
+//
+
+import SwiftUI
+
+struct VerticalLinesOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerticalLinesOverlay()
+}
