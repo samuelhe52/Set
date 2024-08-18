40e92ad18fe813a256cf5b392ec5dee09e21fc66
samuelhe864 <samuelhe52@outlook.com>
2024-08-18 17:53:57 +0800

    Improved macOS compatibility

--- Set/Views/CardView.swift
+++ Set/Views/CardView.swift
@@ -50,10 +50,12 @@ struct CardView: View {
         let gradient = LinearGradient(gradient: baseColor.lighter.brightnessGradient,
                                       startPoint: .topTrailing,
                                       endPoint: .bottomLeading)
-        if !covered {
-            ProportionalRoundedRectangle(cornerFraction: 0.27)
+        
+        let base = ProportionalRoundedRectangle(cornerFraction: 0.27)
+        
+        ZStack {
+            base
                 .stroke(gradient, lineWidth: 3.5)
-                .background(in: ProportionalRoundedRectangle())
                 .aspectRatio(5/7, contentMode: .fit)
                 .overlay(alignment: .center) {
                     VStack {
@@ -68,11 +70,14 @@ struct CardView: View {
                     }
                     .scaleEffect(CGSize(width: 0.65, height: 0.65), anchor: .center)
                 }
-        } else {
-            ProportionalRoundedRectangle(cornerFraction: 0.27)
+            base
                 .fill(gradient)
                 .aspectRatio(5/7, contentMode: .fit)
+                .opacity(covered ? 1 : 0)
         }
+        #if os(macOS)
+        .contentShape(base)
+        #endif
     }
 }
 
